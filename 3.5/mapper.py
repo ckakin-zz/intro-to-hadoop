f9b43fff493581339d300ccd9ec53fc0a7e5b64c
training <training@localhost.localdomain>
2015-08-05 07:24:39 -0400

    add 3.5 answer

---
+++ 3.5/mapper.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# Format of each line is:
+# date\ttime\tstore name\titem description\tcost\tmethod of payment
+#
+# We want elements 2 (store name) and 4 (cost)
+# We need to write them out to standard output, separated by a tab
+
+import sys
+import re
+
+
+for line in sys.stdin:
+   ip = line.split()[0]
+   print ip
+
