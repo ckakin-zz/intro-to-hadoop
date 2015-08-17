baf829a349ddcf55c0eee2c35a7e9de1de523738
training <training@localhost.localdomain>
2015-08-17 11:38:13 -0400

    add student times mapper and reducer

---
+++ final_project/student_times_mapper.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import sys
+import re
+import csv
+from datetime import datetime
+
+reader = csv.reader(sys.stdin, delimiter='\t')
+
+for line in reader:
+  authorId = line[3]
+  if authorId.isdigit(): 
+    addedAt = line[8][:-3]
+    addedAtDate = datetime.strptime(addedAt, "%Y-%m-%d %H:%M:%S.%f") 
+    print "{0}\t{1}".format(authorId, addedAtDate.hour)
---
+++ final_project/student_times_reducer.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+ 
+import sys
+
+# counters used for calculations
+currentId = None
+currentCnt = None
+currentHr = None
+currentMaxCnt = None
+maxHrs = None
+
+## for logging
+isLogOn = 0
+counter = 0
+
+for line in sys.stdin:
+
+  thisId, thisHr =  line.strip().split("\t")
+  
+  if isLogOn:
+    print "Processing line {0}:\tid {1}\thr {2}".format(counter, thisId, thisHr)
+
+  if not currentId:
+    currentId = thisId
+    currentHr = thisHr
+    currentCnt = 1
+    currentMaxCnt = 0
+    maxHrs = []
+  else:
+    if thisId == currentId and thisHr == currentHr: 
+      #id and hr both the same as previous line 
+      currentCnt += 1
+    else:
+      # id or hour different from previous line
+      if currentCnt > currentMaxCnt:
+        maxHrs = [currentHr]
+        currentMaxCnt = currentCnt
+      elif currentCnt == currentMaxCnt:
+        maxHrs.append(currentHr)
+    
+      if(currentId != thisId):
+        # id different from previous line
+        for hr in maxHrs:
+          print "{0}\t{1}".format(currentId, hr)
+        currentId = thisId
+        currentMaxCnt = 0
+        maxHrs = []
+       
+      currentCnt = 1
+      currentHr = thisHr
+
+  counter += 1
+
+for hr in maxHrs:
+  print "{0}\t{1}".format(currentId, hr)
+
+
+
+
